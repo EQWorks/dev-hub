43484ad39b65565be6edc23151a97f09796115c9
Sam Sverko <sam.sverko@gmail.com>
2020-10-19 14:07:09 -0400

    Constants - Create export of url_prefix - Update _app and index to use url_prefix constant. - Index - Remove boilerplate page content.

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,13 @@
 import React from 'react'
 
+import { URL_PREFIX } from '../utils/constants'
 import styles from '../styles/Home.module.css'
-
-const PREFIX = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Welcome to the<br />EQ&nbsp;Works Dev&nbsp;Hub</h1>
       </main>
 
       <footer className={styles.footer}>
@@ -56,7 +18,7 @@ export default function Home() {
         >
           Powered by{' '}
           <img
-            src={`${PREFIX}/vercel.svg`}
+            src={`${URL_PREFIX}/vercel.svg`}
             alt="Vercel Logo"
             className={styles.logo}
           />
