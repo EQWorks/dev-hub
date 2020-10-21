8c228af0bf60736078dbd7a4a1e238cd21fdb3bf
Sam Sverko <sam.sverko@gmail.com>
2020-10-21 09:29:18 -0400

    Updates

---
+++ precache-manifest.2254e74d4c0ee061c66d4d00676e41b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c661d060c08d5204e7d8d9e013a1b9d",
+    "url": "/dev-hub/index.html"
+  },
+  {
+    "revision": "6409a33da0d6e5a92608",
+    "url": "/dev-hub/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "01c2ab8ae054640b4314",
+    "url": "/dev-hub/static/js/2.a153f822.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/dev-hub/static/js/2.a153f822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6409a33da0d6e5a92608",
+    "url": "/dev-hub/static/js/main.c20cf573.chunk.js"
+  },
+  {
+    "revision": "afe77d2502130b1df28a",
+    "url": "/dev-hub/static/js/runtime-main.62375314.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev-hub/static/media/logo.5d5d9eef.svg"
+  }
+]);
